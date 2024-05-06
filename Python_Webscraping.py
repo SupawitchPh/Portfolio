255346f68d6a34c3db14d47d03059d667debaba7
SupawitchPh <128880704+SupawitchPh@users.noreply.github.com>
2024-05-06 16:12:19 +0700

    Create Python_Webscraping.py

---
+++ Python_Webscraping.py
@@ -0,0 +1,27 @@
+!pip install gazpacho
+
+import requests as re
+import pandas as pd
+import gazpacho as gaz
+import csv
+
+url = "https://www.billboard.com/charts/billboard-global-200/"
+response = re.get(url)
+billboard = gaz.Soup(response.text)
+
+songs = billboard.find("h3", {"id": "title-of-a-story", "class": "c-title  a-no-trucate a-font-primary-bold-s"})
+songs_name = [song.strip() for song in songs]
+
+artists = billboard.find("span", {"class": "c-label  a-no-trucate"})
+artists_name = [artist.strip() for artist in artists]
+
+numbers = [num for num in range(1, 201)]
+print(numbers)
+
+df = pd.DataFrame({
+    "Rank": numbers,
+    "Title": songs_name,
+    "Artist": artists_name
+})
+
+df.to_csv("Billboard_Global_200.csv", index = False)
